01725b686936370800620d536756a962e002625c
LouayAbdelkader <louayyabdelkader@gmail.com>
2024-11-24 09:13:44 -0800

    Your commit message

--- Micro-services/user-service/index.js
+++ Micro-services/user-service/index.js
@@ -13,7 +13,7 @@ app.use(bodyParser.json());
 
 
 // Connect to MongoDB
-mongoose.connect('mongodb://localhost:27017/mydatabase')
+mongoose.connect('mongodb://mongo-service:27017/mydatabase')
     .then(() => console.log('MongoDB connected'))
     .catch(err => console.log(err));
 
@@ -21,7 +21,7 @@ mongoose.connect('mongodb://localhost:27017/mydatabase')
 app.use('/api/auth', authRoutes);
 
 // Start the server
-const PORT = process.env.PORT || 5000;
+const PORT = 5000;
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
 });
