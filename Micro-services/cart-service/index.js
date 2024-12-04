17daaae7a76e6b8db2233dbec36ee52cfcd315ba
Louay Abdelkader <121101761+LouayAbdelkader@users.noreply.github.com>
2024-12-04 08:04:38 +0100

    Update index.js

--- Micro-services/cart-service/index.js
+++ Micro-services/cart-service/index.js
@@ -9,7 +9,7 @@ app.use(cors());
 app.use(bodyParser.json());
 
 // Connect to MongoDB
-mongoose.connect('mongodb://localhost:27017/mydatabase', {
+mongoose.connect('mongodb://mongo-service:27017/mydatabase', {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 })
